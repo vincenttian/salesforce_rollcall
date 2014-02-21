a079fc811b686b5d00ce488f60ada02c381fbe46
Vincent Tian <vincenttian16@gmail.com>
2014-02-21 10:12:03 -0800

    Changed backend from custom objects to builtin

--- RollCall.java
+++ RollCall.java
@@ -1,94 +1,76 @@
+/**
+*
+*Roll Call application basic functionality
+*@author Vincent Tian
+*Version1
+*
+*/
+
+
 public class RollCall{
 
 
     // For debugging purposes
     public void inspect_event_table() {
-        Event__c[] table = [SELECT Name__c, Description__c, Time__c FROM Event__c];
+        Event[] table = [SELECT Name, Description, StartDate FROM Event];
         System.Debug(table);
     }
 
     public void inspect_event_attendee_table() {
-        Event_Attendee__c[] table = [SELECT Attendee__c, Event__c, Status__c FROM Event_Attendee__c];
+        CampaignMember[] table = [SELECT Email, FirstName, LastName, Campaign, Status FROM CampaignMember];
         System.Debug(table);
     }
-
-    public void inspect_attendee_table() {
-        Attendee__c[] table = [SELECT First_Name__c, Last_Name__c, Company__c, email__c FROM Attendee__c];
-        System.Debug(table);
-    }
-
 
 
 
     // METHODS THAT INTERACT WITH EVENT TABLE
-    public void create_event(String name, Datetime start_time, String description, Boolean open) {
-        Event__c new_event = new Event__c(Name__c=name, Time__c=start_time, Description__c=description, Open__c=open);
+    public void create_event(String name, Datetime start_time, String description, Status open_status) {
+        Campaign new_event = new Campaign(Name = name, StartDate=start_date, Description=description, Status=open_status);
         insert new_event;
     }
 
 
     public void edit_event(String event_name, String field_name, String value) {
-        Event__c event = [SELECT Name__c, Description__c, Time__c FROM Event__c WHERE Name__c=:event_name];
+        Campaign event = [SELECT Name, Description, StartDate, Status FROM Campaign WHERE Name=:event_name];
         if (field_name == 'description') {
-            event.Description__c = value;
+            event.Description = value;
         }
         update event;
     }
 
     public void delete_event(String event_name) {
-        Event__c event = [SELECT Description__c FROM Event__c WHERE Name__c=:event_name];
+        Campaign event = [SELECT Description FROM Campaign WHERE Name=:event_name];
         delete event;
     }
 
 
 
     // METHODS THAT INTERACT WITH EVENT_ATTENDEE TABLE
-    public void register_event_attendee(String email, String event) {
-        Event_Attendee__c new_event_attendee = new Event_Attendee__c(Attendee__c=email, Event__c=event, Status__c='registered');
+    public void register_event_attendee(Email email, String first_name, String last_name, Campaign event, Status status) {
+        CampaignMember new_event_attendee = new CampaignMember(Email = email, FirstName = first_name, LastName = last_name, Campaign = event, Status = status);
         insert new_event_attendee;
     }
 
-    public void check_in(String event_name, String email) {        
-        Event__c event = [SELECT Name__c FROM Event__c WHERE Name__c=:event_name];
-        String temp_event_name = event.Name__c;
-        Attendee__c name = [SELECT Email__c FROM Attendee__c WHERE Email__c=:email];
-        if (name == null) { // considers if person is a drop-in that is not in the master Attendee table
-            // redirect to add_attendee() so that user enters first name, last name, email, company
-        } else { // the user is already in the master Attendee table
-            String temp_email = name.Email__c;
-            Event_Attendee__c event_attendee = [SELECT Status__c FROM Event_Attendee__c WHERE Attendee__c=:email AND Event__c=:temp_event_name];            
-            event_attendee.Status__c = 'checked in';
-            update event_attendee;
+    public void check_in(Campaign event, String email) {        
+        CampaignMember event_attendee = [SELECT Email, FirstName, LastName, Campaign, Status FROM CampaignMember WHERE Campaign:= event AND Email=:email];
+        if (event_attendee == null) {
+            // THROW ERROR
+            throw new Checkin_Exception('Attendee is not registered for the event');
         }
+        Status new_status = new Status(Status.Received); // Change after creating new status in backend
+        event_attendee.Status = new_status;
+        update new_status; 
     }
 
-    public void delete_event_attendee(String email, String event) {
-        // needs testing
-        Event_Attendee__c event_attendee = [SELECT Attendee__c, Event__c FROM Event_Attendee__c WHERE Attendee__c=:email AND Event__c=:event];
+    public void delete_event_attendee(String email, Campaign event) {
+        CampaignMember event_attendee = [SELECT Email FROM CampaignMember WHERE Email=:email AND Campaign=:event];
         delete event_attendee;
     }
 
+}
 
-
-    // METHODS THAT INTERACT WITH ATTENDEE TABLE
-    public void add_attendee(String first_name, String last_name, String email, String company) {
-        Attendee__c new_attendee = new Attendee__c(First_Name__c=first_name, Last_Name__c=last_name, Email__c=email, Company__c=company);
-        insert new_attendee;
-    }
-
-    public void update_attendee_info(String email, String first_name, String last_name, String company) {
-        // TODO
-        Attendee__c attendee = [SELECT First_Name__c, Last_Name__c, Company__c FROM Attendee__c WHERE Email__c=:email];
-        attendee.First_Name__c = first_name;
-        attendee.Last_Name__c = last_name;
-        attendee.Company__c = company;
-        update attendee;
-    }
-
-    public void delete_attendee(String email) {
-        Attendee__c attendee = [SELECT Email__c FROM Attendee__c WHERE Email__c=:email];
-        delete attendee;
-    }
+// custom exception for handling Event_Attendee checkins
+public class Checkin_Exception extends Exception {
 
 }
 
