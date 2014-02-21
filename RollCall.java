7cdca32ee51e63fc0d90a9f1bbeade29c1e51e5c
Vincent Tian <vincenttian16@gmail.com>
2014-02-21 15:01:46 -0800

    Accomodated new modelling of CampaignMember with lead/contact

--- RollCall.java
+++ RollCall.java
@@ -7,33 +7,37 @@
 */
 
 
-public class RollCall{
+global class RollCall{
 
 
     // For debugging purposes
-    public void inspect_event_table() {
+    // Tested.
+    public static void inspect_event_table() {
         Campaign[] table = [SELECT Name, Description, StartDate FROM Campaign];
         System.Debug(table);
     }
 
-    public void inspect_event_attendee_table() {
-        CampaignMember[] table = [SELECT Email, FirstName, LastName, Campaign, Status FROM CampaignMember];
+    // Tested.
+    public static void inspect_event_attendee_table() {
+        CampaignMember[] table = [SELECT Lead.Email, Lead.FirstName, Lead.LastName, Contact.Email, Contact.FirstName, Contact.LastName, CampaignID, Status FROM CampaignMember];
         System.Debug(table);
     }
 
-    public void inspect_event_table(Campaign event) {
-        CampaignMember[] table = [SELECT Email, FirstName, LastName, Campaign, Status FROM CampaignMember WHERE Campaign=:event];
+    public static void inspect_event_table(Campaign event) {
+        CampaignMember[] table = [SELECT Lead.Email, Lead.FirstName, Lead.LastName, Contact.Email, Contact.FirstName, Contact.LastName, CampaignID, Status FROM CampaignMember];
         System.Debug(table);
     }
+
 
 
     // METHODS THAT INTERACT WITH EVENT TABLE
-    public void create_event(String name, Datetime start_time, String description, Status open_status) {
-        Campaign new_event = new Campaign(Name = name, StartDate=start_date, Description=description, Status=open_status);
+    // Tested
+    public void create_event(String name, Date start_date, String description, String open_status) {
+        Campaign new_event = new Campaign(Name = name, StartDate=start_date, Description=description, Status=open_status, isActive=True);
         insert new_event;
     }
 
-
+    // Tested
     public void edit_event(String event_name, String field_name, String value) {
         Campaign event = [SELECT Name, Description, StartDate, Status FROM Campaign WHERE Name=:event_name];
         if (field_name == 'description') {
@@ -42,6 +46,7 @@ public class RollCall{
         update event;
     }
 
+    // Tested
     public void delete_event(String event_name) {
         Campaign event = [SELECT Description FROM Campaign WHERE Name=:event_name];
         delete event;
@@ -50,34 +55,56 @@ public class RollCall{
 
 
     // METHODS THAT INTERACT WITH EVENT_ATTENDEE TABLE
-    public void register_event_attendee(Email email, String first_name, String last_name, Campaign event, Status status) {
-        CampaignMember new_event_attendee = new CampaignMember(Email = email, FirstName = first_name, LastName = last_name, Campaign = event, Status = status);
+    // Tested
+    public void register_event_attendee(String email, String first_name, String last_name, Campaign event, String status) {
+        // first create contact, then put it in
+        Contact tmp_contact = new Contact(Email=email, FirstName=first_name, LastName=last_name);
+        insert tmp_contact;
+        CampaignMember new_event_attendee = new CampaignMember(ContactId=tmp_contact.id, CampaignId=event.id, Campaign = event, Status = status);
         insert new_event_attendee;
     }
 
+    // Tested
     public void check_in(Campaign event, String email) {        
-        CampaignMember event_attendee = [SELECT Email, FirstName, LastName, Campaign, Status FROM CampaignMember WHERE Campaign:= event AND Email=:email];
+        CampaignMember event_attendee = [SELECT ContactID, Status FROM CampaignMember WHERE Contact.email=:email or Lead.email=:email];
         if (event_attendee == null) {
             // THROW ERROR
-            throw new Checkin_Exception('Attendee is not registered for the event');
+            // throw new Checkin_Exception('Attendee is not registered for the event');
+        } else {
+            event_attendee.status = 'Received';
         }
-        Status new_status = new Status(Status.Received); // Change after creating new status in backend
-        event_attendee.Status = new_status;
-        update new_status; 
+        update event_attendee; 
     }
 
     public void delete_event_attendee(String email, Campaign event) {
-        CampaignMember event_attendee = [SELECT Email FROM CampaignMember WHERE Email=:email AND Campaign=:event];
+        CampaignMember event_attendee = [SELECT Contact.Email, Lead.Email FROM CampaignMember WHERE Contact.Email=:email];
         delete event_attendee;
+    }
+
+
+
+    // METHODS FOR JAVASCRIPT REMOTING
+    @RemoteAction
+    global static Campaign[] get_events() {
+        Campaign[] events = [SELECT Name, Description, StartDate FROM Campaign WHERE isActive=True]; 
+        // AND Status in ('Open', 'In Progress') ];
+        return events;
     }
 
 }
 
 // custom exception for handling Event_Attendee checkins
-public class Checkin_Exception extends Exception {}
+// public class Checkin_Exception extends Exception {}
 
 // Debug script
+/*
+date myDate = date.newInstance(1960, 2, 17);Rollcall test = new Rollcall();
+Campaign x = test.create_event('party', mydate, 'description', 'Open');
+test.register_event_attendee('test@test.com', 'blah', 'tian', x, 'Open');
+Rollcall.inspect_event_attendee_table();
 
+CampaignMember event_attendee = [SELECT Contact.Email, Lead.Email FROM CampaignMember WHERE Contact.Email=:email];
 
+*/
 
 
