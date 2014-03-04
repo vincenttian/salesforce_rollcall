942a4857dd49d0c939902ec419acabb959ef3323
Vincent Tian <vincenttian16@gmail.com>
2014-03-03 17:28:27 -0800

    only checked in on list

--- EventController.java
+++ EventController.java
@@ -103,7 +103,7 @@ global class EventController{
     public CampaignMember[] getAttendees() {
         Campaign c = [SELECT Id FROM Campaign WHERE isActive=True AND Id=:event.cid];
         Map<Id, Campaign> potential_children = new Map<Id, Campaign>([SELECT Name, Description, StartDate, Status, ParentId, Id FROM Campaign WHERE ParentId=:c.id OR Id=:c.id]);
-        CampaignMember[] registered = [SELECT Lead.Firstname, Lead.Lastname, Lead.Email, Contact.Firstname, Contact.Lastname, Contact.Email, Contact.Company__c FROM CampaignMember WHERE CampaignId in :potential_children.keySet()];
+        CampaignMember[] registered = [SELECT Lead.Firstname, Lead.Lastname, Lead.Email, Contact.Firstname, Contact.Lastname, Contact.Email, Contact.Company__c FROM CampaignMember WHERE Status='Responded' AND CampaignId in :potential_children.keySet()];
         return registered;
     }
     // METHODS FOR JAVASCRIPT REMOTING
