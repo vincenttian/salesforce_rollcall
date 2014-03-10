c38948211af8b8f57a37599f2fd17dfff51e8f20
Howard Chen <howardxchen@gmail.com>
2014-03-10 16:55:05 -0700

    Made remote method global

--- EventController.java
+++ EventController.java
@@ -162,7 +162,7 @@ global class EventController{
 
     // For d3    
     @RemoteAction
-    public static List<DateTime> getCheckedInTimes(String event_id) {
+    global static List<DateTime> getCheckedInTimes(String event_id) {
         Campaign c = [SELECT Id FROM Campaign WHERE isActive=True AND Id=:event_id];
         Map<Id, Campaign> potential_children = new Map<Id, Campaign>([SELECT Name, Description, StartDate, Status, ParentId, Id FROM Campaign WHERE ParentId=:c.id OR Id=:c.id]);
         CampaignMember[] checked_in = [SELECT LastModifiedDate, Lead.Firstname, Lead.Lastname, Lead.Email, Contact.Firstname, Contact.Lastname, Contact.Email, Contact.Company__c FROM CampaignMember WHERE Status='Responded' AND CampaignId in :potential_children.keySet() ORDER BY LastModifiedDate ASC];
