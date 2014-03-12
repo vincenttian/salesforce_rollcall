0c919f5cc670ad836aabf3baeafb04d0f7730345
Vincent Tian <vincenttian16@gmail.com>
2014-03-11 22:00:55 -0700

    clean up checkin controller

--- CheckInController.java
+++ CheckInController.java
@@ -14,39 +14,14 @@ global class CheckInController{
         event = [SELECT Id, Name, Description, StartDate FROM Campaign WHERE Id=:ApexPages.currentPage().getParameters().get('event_id')];
     }
 
-
-    // For debugging purposes
-    // Tested.
-    public static void inspect_event_table() {
-        Campaign[] table = [SELECT Name, Description, StartDate FROM Campaign];
-        System.Debug(table);
-    }
-
-    // Tested.
-    public static void inspect_event_attendee_table() {
-        CampaignMember[] table = [SELECT Lead.Email, Lead.FirstName, Lead.LastName, Contact.Email, Contact.FirstName, Contact.LastName, CampaignID, Status FROM CampaignMember];
-        System.Debug(table);
-    }
-
-    public static void inspect_event_table(Campaign event) {
-        CampaignMember[] table = [SELECT Lead.Email, Lead.FirstName, Lead.LastName, Contact.Email, Contact.FirstName, Contact.LastName, CampaignID, Status FROM CampaignMember];
-        System.Debug(table);
-    }
-
-
-    // Case; Used when drop-ins come to an event
-    public static void register_event_attendee(String email, String first_name, String last_name, String company, String campaign_id) { // Later switch to just contact and campaign ID
-        // first create contact, then put it in
+    public static void register_event_attendee(String email, String first_name, String last_name, String company, String campaign_id) { 
         Contact tmp_contact = new Contact(Email=email, FirstName=first_name, LastName=last_name, Company__c=company);
         insert tmp_contact;
-        CampaignMember new_event_attendee = new CampaignMember(ContactId=tmp_contact.id, CampaignId=campaign_id, Status = 'Responded'); // NOT SETTING PICKLIST TO DEFAULT SPECIFIED VALUE
+        CampaignMember new_event_attendee = new CampaignMember(ContactId=tmp_contact.id, CampaignId=campaign_id, Status = 'Responded'); 
         insert new_event_attendee;
     }
 
-    public static void update_event_attendee(String email, String first_name, String last_name, String company, String campaign_id) { // Later switch to just contact and campaign ID
-        // first create contact, then put it in
-        
-        // NEEDS WORK FIX THIS 
+    public static void update_event_attendee(String email, String first_name, String last_name, String company, String campaign_id) { 
         CampaignMember member = [SELECT ContactId FROM CampaignMember WHERE CampaignId=:campaign_id AND (Contact.Email=:email or Lead.Email=:email)];
         Contact updated_contact = [SELECT firstname, lastname, Company__c FROM Contact WHERE Id=:member.ContactId];
         updated_contact.firstname = first_name;
@@ -55,27 +30,16 @@ global class CheckInController{
         update updated_contact;
     }
 
-    // Tested
     public static String[] check_in(String campaign_id, String email) {        
         CampaignMember event_attendee = [SELECT ContactID, Status, CampaignId FROM CampaignMember WHERE CampaignId=:campaign_id AND (Contact.email=:email or Lead.email=:email)];
-        System.debug('in call');
         if (event_attendee == null) {
-            System.debug('in call');
-            // THROW ERROR
-            // throw new Checkin_Exception('Attendee is not registered for the event');
             return new String[0];
         } else {
-            System.debug(event_attendee);
-            // System.debug(event_attendee[0].status);
-            event_attendee.status = 'Responded'; // temp status to signify checked in  // NOT SETTING PICKLIST TO SPECIFIED VALUE
-            System.debug(event_attendee);
+            event_attendee.status = 'Responded'; 
         }
         update event_attendee;
-
-
         Contact contact_event_attendee = [SELECT FirstName, LastName, Company__c FROM Contact WHERE Id=:event_attendee.ContactId];
         String[] info = new String[3];
-        // String[] info = new String{contact_event_attendee.FirstName, contact_event_attendee.LastName, contact_event_attendee.Company__c};
         info[0] = contact_event_attendee.FirstName;
         info[1] = contact_event_attendee.LastName;
         info[2] = contact_event_attendee.Company__c;
@@ -83,17 +47,12 @@ global class CheckInController{
     }
 
     // logic to check if a campaign member needs to register or just check in
-    // first check if the person is registered or not
     public static String[] handle_parent_events(String campaign_id, String email) {
         Map<Id, Campaign> potential_children = new Map<Id, Campaign>([SELECT Name, Description, StartDate, Status, ParentId, Id FROM Campaign WHERE ParentId=:campaign_id OR Id=:campaign_id]);
         CampaignMember[] event_attendee = [SELECT Id, CampaignId, ContactId FROM CampaignMember WHERE CampaignId in :potential_children.keySet() AND (Lead.Email=:email OR Contact.Email=:email)];
         if (event_attendee.size() == 0) {
-            System.debug('No Attendees');
-            // register the attendee
-            // MUST RAISE AN ERROR
             return new String[0];
         } else if (event_attendee.size() != 1) {
-            // String[] info = new String[3*event_attendee.size()];
             String[] info = new String[6];
             Contact[] contact_event_attendee = [SELECT FirstName, LastName, Company__c FROM Contact WHERE (Id=:event_attendee[0].ContactId or Id=:event_attendee[1].ContactId)];
             info[0] = contact_event_attendee[0].FirstName;
@@ -112,7 +71,6 @@ global class CheckInController{
     @RemoteAction
     global static String[] check_in_attendee(String event_id, String email) {
         Campaign event = [SELECT Id FROM Campaign WHERE isActive=True AND Id=:event_id];
-        System.debug('Remote call');
         return CheckInController.handle_parent_events(string.valueof(event.Id), email);
     }
 
@@ -139,15 +97,3 @@ global class CheckInController{
     }
 
 }
-
-// Debug script
-
-/*
-
-date myDate = date.newInstance(1960, 2, 17);EventController test = new EventController();
-Campaign x = test.create_event('party', mydate, 'description', 'Open');
-test.register_event_attendee('test@test.com', 'blah', 'tian', x, 'Open');
-Rollcall.inspect_event_attendee_table();
-CampaignMember event_attendee = [SELECT Contact.Email, Lead.Email FROM CampaignMember WHERE Contact.Email=:email];
-
-*/
