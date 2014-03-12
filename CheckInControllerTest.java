8bd7302e4f4be4460f9bc53ac4f174d5d21c8545
Vincent Tian <vincenttian16@gmail.com>
2014-03-11 22:13:29 -0700

    correct outline for tests that should be tested

---
+++ CheckInControllerTest.java
@@ -0,0 +1,16 @@
+@isTest 
+private class CheckInControllerTest {
+    
+    static testMethod void test_update_event_attendee() {
+
+    }
+
+    static testMethod void test_register_event_attendee() {
+
+    }
+
+    static testMethod void test_check_in() {
+
+    }
+
+}
--- EventControllerTest.java
+++ EventControllerTest.java
@@ -1,76 +1,16 @@
 @isTest 
-private class RollCallTest {
+private class EventControllerTest {
     
     static testMethod void test_attendee_search() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
+
     }
 
-    static testMethod void test_get_event() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
-    }
+    static testMethod void test_get_attendees() {
 
-    static testMethod void get_event_date() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
-    }
-
-    static testMethod void test_edit_info() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
-    }
-
-    static testMethod void test_event_stats() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
     }
 
     static testMethod void test_get_checkedin_times() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        Campaign c = create_event('test_event1', test_date, 'test_description', 'Open');
-        System.debug('Inserting campaign into database: ' + c);
-        // Insert 
-        insert c;
-        // Retrieve 
-        c = [SELECT Name, StartDate, Description, Status, isActive FROM Campaign WHERE Id =:c.Id];
-        // Test that the trigger correctly updated the price
-        System.assertEquals(True, c.isActive);
+
     }
 
 }
