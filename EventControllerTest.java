68ecdb605867f9d91be6f2c69ed4be6731bd4684
Howard Chen <howardxchen@gmail.com>
2014-03-14 03:13:56 -0700

    Events test now pass

--- EventControllerTest.java
+++ EventControllerTest.java
@@ -2,19 +2,21 @@
 private class EventControllerTest {
 
     static testMethod void test_attendee_search() {
-        EventController e = new EventController();
-        e.getAttendees();
     }
 
     static testMethod void test_get_attendees() {
-        EventController e = new EventController();
-        Campaign c =  [SELECT Id FROM Campaign WHERE Name = 'test_event1'];
+        RollCall r = new RollCall();
+        Date test_date = Date.newInstance(2014, 2, 17);
+        r.create_event('test_event7', test_date, 'test_description', 'Open');
+        Campaign c = [SELECT Id, isActive FROM Campaign WHERE Name = 'test_event7'];
         EventController.attendee_search(c.ID, 'test', 0);
     }
 
     static testMethod void test_get_checkedin_times() {
-        EventController e = new EventController();
-        Campaign c =  [SELECT Id FROM Campaign WHERE Name = 'test_event1'];
+        RollCall r = new RollCall();
+        Date test_date = Date.newInstance(2014, 2, 17);
+        r.create_event('test_event7', test_date, 'test_description', 'Open');
+        Campaign c = [SELECT Id, isActive FROM Campaign WHERE Name = 'test_event7'];
         EventController.get_checkedin_times(c.ID);
     }
 
