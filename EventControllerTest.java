c65f07d9ed1a855041a637c9121f0051ab6553aa
Howard Chen <howardxchen@gmail.com>
2014-03-14 03:04:17 -0700

    Crappy tests for EventController

--- EventControllerTest.java
+++ EventControllerTest.java
@@ -1,16 +1,21 @@
-@isTest 
+@isTest
 private class EventControllerTest {
-    
-    static testMethod void test_attendee_search() {
 
+    static testMethod void test_attendee_search() {
+        EventController e = new EventController();
+        e.getAttendees();
     }
 
     static testMethod void test_get_attendees() {
-
+        EventController e = new EventController();
+        Campaign c =  [SELECT Id FROM Campaign WHERE Name = 'test_event1'];
+        EventController.attendee_search(c.ID, 'test', 0);
     }
 
     static testMethod void test_get_checkedin_times() {
-
+        EventController e = new EventController();
+        Campaign c =  [SELECT Id FROM Campaign WHERE Name = 'test_event1'];
+        EventController.get_checkedin_times(c.ID);
     }
 
 }
