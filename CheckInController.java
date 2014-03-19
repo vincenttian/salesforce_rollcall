0fbc2375fee448e514b91ded9989b95963d6c939
Vincent Tian <vincenttian16@gmail.com>
2014-03-19 14:10:40 -0700

    initial settings page and finished check in logic for max capacity

--- CheckInController.java
+++ CheckInController.java
@@ -51,26 +51,27 @@ global class CheckInController{
     // logic to check if a campaign member needs to register or just check in
     public static CampaignMember[] handle_parent_events(String campaign_id, String email) {
         Map<Id, Campaign> potential_children = new Map<Id, Campaign>([SELECT Name, Description, StartDate, Status, ParentId, Id FROM Campaign WHERE ParentId=:campaign_id OR Id=:campaign_id]);
-        Integer checkedIn = [SELECT Count() FROM CampaignMember
+        Campaign pcampaign = [SELECT MaxCapacity__c FROM Campaign WHERE Id=: campaign_id];
+        Decimal capacity = pcampaign.MaxCapacity__c;
+        if (capacity != null) { // There is a max capacity
+            Integer checkedIn = [SELECT Count() FROM CampaignMember
                      WHERE CampaignId in :potential_children.keySet() AND
                      (Status=:checkedInStatus)];
-        Campaign pcampaign = [SELECT MaxCapacity__c FROM Campaign WHERE Id=: campaign_id];
-        Integer capacity = pcampaign.MaxCapacity__c.intValue();
-        // MANY TIMES, MAX CAPACITY WILL NOT MATTER
-        System.debug('checked in: ' + checkedIn);
-        System.debug('capacity: ' + capacity);
-        if (checkedIn >= capacity) {
-            System.debug('got to capacity case');
-            throw new CapacityException('Event already at max capacity');
-        }
-        CampaignMember[] event_attendee = [SELECT Id, CampaignId, ContactId, LeadId, Lead.Name, Lead.FirstName, Lead.LastName, Contact.Name, Contact.FirstName, Contact.LastName, Lead.Email, Contact.Email, Lead.Company, Contact.Company__c
-                                           FROM CampaignMember WHERE CampaignId in :potential_children.keySet() AND
-                                           (Lead.Email=:email OR Contact.Email=:email)];
-        if (event_attendee.size() == 1) {            
-            check_in(event_attendee[0]);
-        }
-
-        return event_attendee;
+            Integer capacity_int = pcampaign.MaxCapacity__c.intValue();
+            if (checkedIn >= capacity_int) {
+                throw new CapacityException('Event already at max capacity');
+            } else {
+                return new CampaignMember[0];
+            }
+        } else {
+            CampaignMember[] event_attendee = [SELECT Id, CampaignId, ContactId, LeadId, Lead.Name, Lead.FirstName, Lead.LastName, Contact.Name, Contact.FirstName, Contact.LastName, Lead.Email, Contact.Email, Lead.Company, Contact.Company__c
+                                               FROM CampaignMember WHERE CampaignId in :potential_children.keySet() AND
+                                               (Lead.Email=:email OR Contact.Email=:email)];
+            if (event_attendee.size() == 1) {            
+                check_in(event_attendee[0]);
+            }
+            return event_attendee;
+        }      
     }
 
     // Checking in attendees for checkin page
---
+++ SettingsController.java
@@ -0,0 +1,8 @@
+public class SettingsController{
+    
+    @RemoteAction
+    global static Event update_settings(String event_id) {
+        // Do stuff
+    }
+
+}
