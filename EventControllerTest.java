cb9cd6e7ac39d6d907917e19d892de7e000a1515
Howard Chen <howardxchen@gmail.com>
2014-03-26 12:12:22 -0700

    Update event controller tests

--- EventControllerTest.java
+++ EventControllerTest.java
@@ -3,7 +3,21 @@ private class EventControllerTest {
 
     static testMethod void test_attendee_search() {
         Campaign c = RollCallTestUtility.createEventCampaign();
-        Event[] events = EventController.attendee_search(c.ID, 'Lee', 0);
+        SObject[] events = EventController.attendee_search(c.ID, 'Lee', 0);
         System.assertNotEquals(events, null);
+    }
+    
+    static testMethod void test_get_checkedin_times() {
+        Campaign c = RollCallTestUtility.createEventCampaign();
+        String cid = c.ID + '';
+        List<DateTime> dates = EventController.get_checkedin_times(cid);
+        System.assertNotEquals(dates, null);
+    }
+    
+    static testMethod void test_update_stats() {
+        Campaign c = RollCallTestUtility.createEventCampaign();
+        String cid = c.ID + '';
+        Event e = EventController.update_stats(cid);
+        System.assertNotEquals(e, null);
     }
 }
