1d3c228ec09d0b156ac1d3b05aa9288909c9d2ff
Howard Chen <howardxchen@gmail.com>
2014-03-26 11:32:47 -0700

    Update RollCallTests

--- RollCallTest.java
+++ RollCallTest.java
@@ -1,51 +1,16 @@
 @isTest
 private class RollCallTest {
 
-    static testMethod void test_create_event() {
-        Date test_date = Date.newInstance(2014, 2, 17);
-        RollCall r = new RollCall();
-        r.create_event('test_event1', test_date, 'test_description', 'Open');
-        Campaign parent_campaign =  [SELECT Name FROM Campaign WHERE Name = 'test_event1'];
-        System.assertNotEquals(null, parent_campaign);
-    }
-
-    static testMethod void test_create_child_event() {
-        RollCall r = new RollCall();
-        Date test_date = Date.newInstance(2014, 2, 17);
-        r.create_event('test_event2', test_date, 'test_description', 'Open');
-        Campaign parent_campaign =  [SELECT Id FROM Campaign WHERE Name = 'test_event2'];
-        test_date = Date.newInstance(2014, 2, 17);
-        r.create_child_event('test_event2', test_date, 'test_description', 'Open', parent_campaign.Id);
-        Campaign child_event = [SELECT Id FROM Campaign WHERE ParentID =: parent_campaign.Id];
-        System.assertNotEquals(null, child_event);
-    }
-
-    static testMethod void test_delete_event() {
-        RollCall r = new RollCall();
-        Date test_date = Date.newInstance(2014, 2, 17);
-        r.create_event('test_event3', test_date, 'test_description', 'Open');
-        Campaign c = [SELECT Id FROM Campaign WHERE Name = 'test_event3'];
-        r.delete_event(c.Id);
-        Campaign[] d;
-        //c = [SELECT Id FROM Campaign WHERE Name = 'test_event3'];
-        System.assertEquals(null, d);
-    }
-
-    static testMethod void test_end_event() {
-        RollCall r = new RollCall();
-        Date test_date = Date.newInstance(2014, 2, 17);
-        r.create_event('test_event7', test_date, 'test_description', 'Open');
-        Campaign c = [SELECT Id, isActive FROM Campaign WHERE Name = 'test_event7'];
-        System.assertEquals(True, c.isActive);
-        r.end_event(c.Id);
-        //System.assertEquals(False, c.isActive);
-    }
-
-    static testMethod void test_get_events() {
-        RollCall r = new RollCall();
-        Date test_date = Date.newInstance(2014, 2, 17);
-        r.create_event('test_event1', test_date, 'test_description', 'Open');
-        Event[] e = r.getEvents();
-        System.assertNotEquals(null, e);
+    static testMethod void test() {
+        RollCallTestUtility.createEventCampaign();
+        Rollcall r = new RollCall();
+        Event[] events = r.getEvents();
+        boolean containsMember = false;
+        for (Event e: events) {
+            if (e.registered == 4 && e.name =='Parent') {
+                 containsMember = true;
+            }
+        }
+        System.assertEquals(containsMember, true);
     }
 }
---
+++ RollCallTestUtility.java
@@ -0,0 +1,27 @@
+@isTest
+public class RollCallTestUtility{
+    
+    public static Campaign createEventCampaign() {
+        Campaign c = new Campaign(name='Parent', startdate=Date.today(), isActive=true);
+        insert c;
+        Campaign c2 = new Campaign(name='Child', startdate=Date.today(), isActive=true, ParentID=c.ID);
+        insert c2;
+        Lead le = new Lead(firstName='Lead', lastName='Lee', company='SF');
+        insert le;
+        CampaignMember m1 = new CampaignMember(LeadId=le.ID, campaignId=c.ID);
+        insert m1;
+        Lead le2 = new Lead(firstName='Lead2', lastName='Lee', company='SF');
+        insert le2;
+        CampaignMember m2 = new CampaignMember(LeadId=le2.ID, campaignId=c.ID);
+        insert m2;
+        Contact cn = new Contact(firstName='Contact', lastName='C');
+        insert cn;
+        CampaignMember m3 = new CampaignMember(ContactId=cn.ID, campaignId=c.ID);
+        insert m3;
+        Contact cn2 = new Contact(firstName='Contact2', lastName='C');
+        insert cn2;
+        CampaignMember m4 = new CampaignMember(ContactId=cn2.ID, campaignId=c2.ID);
+        insert m4;
+        return c;
+    }
+}
