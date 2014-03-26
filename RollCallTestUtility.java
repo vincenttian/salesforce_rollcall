0b068f99cab50deecf489b24ea8092de977671b6
Howard Chen <howardxchen@gmail.com>
2014-03-26 14:30:49 -0700

    Update tests

--- RollCallTestUtility.java
+++ RollCallTestUtility.java
@@ -6,11 +6,11 @@ public class RollCallTestUtility{
         insert c;
         Campaign c2 = new Campaign(name='Child', startdate=Date.today(), isActive=true, ParentID=c.ID);
         insert c2;
-        Lead le = new Lead(firstName='Lead', lastName='Lee', company='SF');
+        Lead le = new Lead(firstName='Lead', lastName='Lee', company='SF', email='test@test.com');
         insert le;
         CampaignMember m1 = new CampaignMember(LeadId=le.ID, campaignId=c.ID);
         insert m1;
-        Lead le2 = new Lead(firstName='Lead2', lastName='Lee', company='SF');
+        Lead le2 = new Lead(firstName='Lead2', lastName='Lee', company='SF', email='a@b.com');
         insert le2;
         CampaignMember m2 = new CampaignMember(LeadId=le2.ID, campaignId=c.ID);
         insert m2;
@@ -18,10 +18,11 @@ public class RollCallTestUtility{
         insert cn;
         CampaignMember m3 = new CampaignMember(ContactId=cn.ID, campaignId=c.ID);
         insert m3;
-        Contact cn2 = new Contact(firstName='Contact2', lastName='C');
+        Contact cn2 = new Contact(firstName='Contact2', lastName='C', email='test@test.com');
         insert cn2;
         CampaignMember m4 = new CampaignMember(ContactId=cn2.ID, campaignId=c2.ID);
         insert m4;
         return c;
     }
+
 }
--- SettingControllerTest.java
+++ SettingControllerTest.java
@@ -6,14 +6,20 @@ private class SettingControllerTest {
         SettingsController s = new SettingsController();
         List<String> types= s.getCampaignTypes();
         System.assertNotEquals(types, null);
+        boolean size = types.size() >= 1;
+        System.assertEquals(size, true);
     }
 
     static testMethod void test_update_settings() {
         String[] info = SettingsController.check_settings();
         System.assertEquals(info[0], 'Responded');
-        SettingsController.update_settings('1', '2', 'All');
+        System.assertEquals(info[1], 'Sent');
+        System.assertEquals(info[2], 'All');
+        SettingsController.update_settings('1', '2', 'None');
         info = SettingsController.check_settings();
         System.assertEquals(info[0], '1');
+        System.assertEquals(info[1], '2');
+        System.assertEquals(info[2], 'None');
 
     }
 
