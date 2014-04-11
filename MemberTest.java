92645119c006c5d8dcf2d7e6d536b92b1e1bc467
Howard Chen <howardxchen@gmail.com>
2014-04-11 09:50:37 -0700

    Update member test to fit member

--- MemberTest.java
+++ MemberTest.java
@@ -2,8 +2,8 @@
 private class MemberTest {
 
     static testMethod void test_member() {
-       Member one = new Member ('A', '', '', false);
-       Member two = new Member ('B', '', '', false);
+       Member one = new Member ('1','A', '', '', false);
+       Member two = new Member ('2','B', '', '', false);
        boolean t = one.compareTo(two) < 0;
        System.assertEquals(t, true);
     }
