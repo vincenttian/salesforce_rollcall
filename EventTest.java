55bbac03dfa66777abb10f0091bb5d484522d163
Howard Chen <howardxchen@gmail.com>
2014-04-11 09:48:52 -0700

    Add test

--- EventTest.java
+++ EventTest.java
@@ -3,9 +3,25 @@ private class EventTest {
 
     static testMethod void test_event() {
         Campaign c = RollCallTestUtility.createEventCampaign();
-        for (int i = 0; i < 12; i++) {
-            Event e = new Event(c.ID, i);
+        Event e = new Event(c);
+        Campaign d;
+        for (Integer i = 0; i < 12; i++) {
+            d = new Campaign(name='TestEvent', StartDate=Date.today().AddMonths(i));
+            e = new Event(d, i);
         }
+        d = new Campaign(name='TestEvent', StartDate=Date.today().AddDays(2));
+        e = new Event(d, 1);
+        System.assertEquals(e.name, d.name);
+    }
+    
+    static testMethod void test_event2() {
+        Campaign c = new Campaign(name='Parent', isActive=true, MaxCapacity__c = 1);
+        insert c;
+        Event e = new Event(c, 0);
+        c = new Campaign(name='Parent', isActive=true, MaxCapacity__c = 1, StartDate=Date.today().AddMonths(1));
+        new Event(c);
+        c = new Campaign(name='Parent', isActive=true, MaxCapacity__c = 1);
+        new Event(c);
     }
 
 }
